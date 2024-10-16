4ca08e29369254c24e29e19fd1c6cb4d2aa392f9
Graf <ilyaantar@yandex.ru>
2024-10-16 22:58:57 +0300

    Страница Stats

---
+++ GravitonEcoWeb/Pages/Stats.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GravitonEcoWeb.Pages
+{
+    public class StatsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ GravitonEcoWeb/wwwroot/js/stats.js
@@ -0,0 +1,22 @@
+﻿// Добавляем обработчик для всех кнопок с классом "tab-button"
+document.querySelectorAll('.tab-button').forEach(button => {
+    button.addEventListener('click', function() {
+        const target = document.querySelector(this.getAttribute('data-target'));
+
+        // Убираем активное состояние у всех кнопок
+        document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
+
+        // Добавляем активное состояние только на нажатую кнопку
+        this.classList.add('active');
+
+        // Закрываем все открытые блоки перед тем, как открыть новый
+        document.querySelectorAll('.collapse-content').forEach(content => {
+            content.classList.remove('open');
+            content.style.maxHeight = '0';
+        });
+
+        // Открываем соответствующий контент
+        target.classList.add('open');
+        target.style.maxHeight = target.scrollHeight + 'px';
+    });
+});
